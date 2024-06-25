f8dc0a4412fe9a06382caa1a0f36a6d95e8af874
SpectacularVoyager <Mail2ankushd@gmail.com>
2024-06-25 19:49:13 +0530

    more email stuff

--- src/main/java/com/ankush/cleancity/MailStuff/MailBean.java
+++ src/main/java/com/ankush/cleancity/MailStuff/MailBean.java
@@ -2,6 +2,7 @@ package com.ankush.cleancity.MailStuff;
 
 import com.ankush.cleancity.Wastes.EmailedWaste;
 import com.ankush.cleancity.Wastes.Waste;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class MailBean {
-    private static final Logger log = LoggerFactory.getLogger(MailBean.class);
     @Autowired
     private JavaMailSender emailSender;
 
